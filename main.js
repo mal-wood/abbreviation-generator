b55593ecef1d51434c7110c82fd6fb14dd06b1a1
mal-wood <mallorywood413@gmail.com>
2017-03-05 00:12:33 -0800

    Add ajax functionality and successfully return response

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  
+  submitButtonListener();
 });
+
+var submitButtonListener = function() {
+  $('#abbrev-button').on("click", function(event){
+    event.preventDefault();
+    
+    var request = $.ajax({
+      url: 'http://www.stands4.com/services/v2/abbr.php?uid=' + MY_ID + '&tokenid=' + MY_TOKEN +'&term=asap',
+      method: 'get'
+    })
+
+    request.done(function(response){
+      console.log(response)
+    })
+
+  })
+}
